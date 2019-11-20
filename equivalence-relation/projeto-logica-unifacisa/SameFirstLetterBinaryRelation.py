a8db026f61a13c47fdfc8ce0fd821eabe6814d31
jxvictor <57672775+jxvictor@users.noreply.github.com>
2019-11-20 16:06:44 -0300

    Update SameFirstLetterBinaryRelation.py

--- equivalence-relation/projeto-logica-unifacisa/SameFirstLetterBinaryRelation.py
+++ equivalence-relation/projeto-logica-unifacisa/SameFirstLetterBinaryRelation.py
@@ -9,22 +9,21 @@ class SameFirstLetterBinaryRelation(BinaryRelation):
     def contains_ordered_pair(self, x, y):
         """
         This method returns a boolean value indicating if both elements of a given ordered pair have the same first letter.
-
         Arguments:
         x - The first element of the ordered pair.
         y - The second element of the ordered pair.
-
         Return True if the ordered pair belongs to the binary relation, otherwise, return False.
         """
-        pass
+        if(x[0] == y[0]):
+            return True
+        else:
+            return False
 
     def relation(self, S):
         """
         This method returns a set of pairs in SxS (a.k.a. S²) that belong to the binary relation.
-
         Arguments:
         S - The input set.
-
         Return a set of pairs in SxS (a.k.a. S²) that belong to the binary relation.
         """
-        pass
+        return set([(x, y) for x in S for y in S if self.contains_ordered_pair(x, y)])
